126787d7df55554748e4dc40b656e216f5384af9
Michael Wimble <mike@wimblerobotics.com>
2024-08-05 09:57:52 -0700

    All dc_dc converters done.

--- scripts/generate_wiring_docs.sh
+++ scripts/generate_wiring_docs.sh
@@ -1,7 +1,6 @@
-``` bash
+#!/bin/bash
 m4 <wiring_all.yml >foo.yml;wireviz --format hp -O sigyn_all__wiring foo.yml
 m4 <wiring_24v_dc_dc.yml >foo.yml;wireviz --format hp -O sigyn_24v_dc_dc__wiring foo.yml
 m4 <wiring_12v_dc_dc.yml >foo.yml;wireviz --format hp -O sigyn_12v_dc_dc__wiring foo.yml
 m4 <wiring_5v_dc_dc.yml >foo.yml;wireviz --format hp -O sigyn_5v_dc_dc__wiring foo.yml
 m4 <wiring_3r3v_dc_dc.yml >foo.yml;wireviz --format hp -O sigyn_3r3v_dc_dc__wiring foo.yml
-```
