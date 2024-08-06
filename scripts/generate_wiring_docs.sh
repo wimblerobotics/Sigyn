761d40951d75576b2221143d1463a5cf8bc0b4a6
Michael Wimble <mike@wimblerobotics.com>
2024-08-05 17:29:02 -0700

    Estop and 24v meter

--- scripts/generate_wiring_docs.sh
+++ scripts/generate_wiring_docs.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 m4 <wiring_all.yml >foo.yml;wireviz --format hp -O sigyn_all__wiring foo.yml
+m4 <wiring_anderson.yml >foo.yml;wireviz --format hp -O sigyn_anderson__wiring foo.yml
+m4 <wiring_control_panel.yml >foo.yml;wireviz --format hp -O sigyn_control_panel__wiring foo.yml
 m4 <wiring_24v_dc_dc.yml >foo.yml;wireviz --format hp -O sigyn_24v_dc_dc__wiring foo.yml
 m4 <wiring_12v_dc_dc.yml >foo.yml;wireviz --format hp -O sigyn_12v_dc_dc__wiring foo.yml
 m4 <wiring_5v_dc_dc.yml >foo.yml;wireviz --format hp -O sigyn_5v_dc_dc__wiring foo.yml
