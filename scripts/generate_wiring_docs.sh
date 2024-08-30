9e6305fddf725169708ee8ae510bde47c168a681
Michael Wimble <mike@wimblerobotics.com>
2024-08-29 19:40:25 -0700

    Wiring for top plate (not including 24v)

--- scripts/generate_wiring_docs.sh
+++ scripts/generate_wiring_docs.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 m4 <wireviz_sigyn.yml >foo.yml;wireviz --format hp -O sigyn_wiring foo.yml
+m4 <wireviz_sigy_anderson.yml >foo.yml;wireviz --format hp -O anderson_wiring foo.yml
+m4 <wireviz_topplate.yml >foo.yml;wireviz --format hp -O topplate_wiring foo.ymlm4
