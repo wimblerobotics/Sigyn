f578b9f4757909be7243ed0f29912e09a55eb509
Michael Wimble <mike@wimblerobotics.com>
2024-10-23 11:10:08 -0700

    Gripper action now working

--- gripper/elevator/tmicro_ros.h
+++ gripper/elevator/tmicro_ros.h
@@ -18,25 +18,20 @@
 #include "sigyn_interfaces/action/move_elevator.h"
 #include "tmodule.h"
 
-class TMicroRos : TModule
-{
+class TMicroRos : TModule {
 public:
   // Check if ROS time appears to be correct and, if not, fix it.
   // Returns a reasonable ROS time.
-  static int64_t FixedTime(const char* caller);
+  static int64_t FixedTime(const char *caller);
 
   // Publish a diagnistoc message.
-  static void PublishDiagnostic(const char* msg);
+  static void PublishDiagnostic(const char *msg);
 
   // Singleton constructor.
-  static TMicroRos& singleton();
+  static TMicroRos &singleton();
 
 protected:
-  enum Direction
-  {
-    kUp,
-    kDown
-  };
+  enum Direction { kUp, kDown };
 
   // Check if the elevator is at the bottom limit.
   static bool ElevatorAtBottomLimit();
@@ -54,10 +49,7 @@ protected:
   void loop();
 
   // From TModule
-  const char* name()
-  {
-    return "uRos";
-  }
+  const char *name() { return "uRos"; }
 
   // From TModule
   void setup();
@@ -67,25 +59,23 @@ protected:
   static void ExtenderStepPulse(Direction upwdirectionrds);
 
 private:
-  enum State
-  {
+  enum State {
     kWaitingAgent,
     kAgentAvailable,
     kAgentConnected,
     kAgentDisconnected
   } state_;
 
-  enum
-  {
+  enum {
     // For limit switches, 0 => interrupted, 1 => not interrupted.
-    kExtenderInLimitSwitchPin = 35,      // Echo 3
-    kExtenderOutLimitSwitchPin = 34,     // Trigger 3
-    kExtenderStepPulsePin = 37,          // Echo 2
-    kExtenderStepDirectionPin = 36,      // Trigger 2
-    kElevatorBottomLimitSwitchPin = 41,  // Echo 1
-    kElevatorTopLimitSwitchPin = 40,     // Trigger 1
-    kElevatorStepPulsePin = 15,     // Echo 0
-    kElevatorStepDirectionPin = 14  // Trigger 0
+    kExtenderInLimitSwitchPin = 35,     // Echo 3
+    kExtenderOutLimitSwitchPin = 34,    // Trigger 3
+    kExtenderStepPulsePin = 37,         // Echo 2
+    kExtenderStepDirectionPin = 36,     // Trigger 2
+    kElevatorBottomLimitSwitchPin = 41, // Echo 1
+    kElevatorTopLimitSwitchPin = 40,    // Trigger 1
+    kElevatorStepPulsePin = 15,         // Echo 0
+    kElevatorStepDirectionPin = 14      // Trigger 0
   };
 
   // Private constructor.
@@ -98,24 +88,26 @@ private:
   static void doElevatorCommand();
   static void doExtenderCommand();
 
-  // Gripper action handler.
-  // static rcl_ret_t HandleGripperGoal(rclc_action_goal_handle_t* goal_handle, void* context);
-  // static bool HandleGripperCancel(rclc_action_goal_handle_t* goal_handle, void* context);
-
-      // Sync ROS time.
-      static void SyncTime(const char* caller, uint32_t fixed_time_call_count);
+  // Sync ROS time.
+  static void SyncTime(const char *caller, uint32_t fixed_time_call_count);
 
   // Regular maintenance, publish stats, etc.
-  static void TimerCallback(rcl_timer_t* timer, int64_t last_call_time);
+  static void TimerCallback(rcl_timer_t *timer, int64_t last_call_time);
 
   // Handler for elevator_cmd messages.
-  static void ElevatorCommandCallback(const void* msg);
+  static void ElevatorCommandCallback(const void *msg);
 
   // Handler for elevator_cmd messages.
-  static void ExtenderCommandCallback(const void* msg);
+  static void ExtenderCommandCallback(const void *msg);
 
-  // For checking for a reasonable ROS time.
-  static volatile int64_t ros_sync_time_;
+  static bool HandleGripperCancel(rclc_action_goal_handle_t *goal_handle,
+                                  void *context);
+
+  static rcl_ret_t HandleGripperGoal(rclc_action_goal_handle_t *goal_handle,
+                                     void *context);
+
+      // For checking for a reasonable ROS time.
+      static volatile int64_t ros_sync_time_;
 
   // Micro-ROS variables
   rcl_allocator_t allocator_;
@@ -138,8 +130,8 @@ private:
   std_msgs__msg__String string_msg_;
 
   // Elevator position;
-  static float current_elevator_position_;  // In meters.
-  static float current_extender_position_;  // In meters.
+  static float current_elevator_position_; // In meters.
+  static float current_extender_position_; // In meters.
 
   // Mm per pulse.
   static const float kElevatorMmPerPulse_;
@@ -151,5 +143,5 @@ private:
   static bool extender_has_command_;
 
   // Singleton instance.
-  static TMicroRos* g_singleton_;
+  static TMicroRos *g_singleton_;
 };
