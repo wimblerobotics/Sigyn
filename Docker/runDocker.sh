3c631533335b319b6e2c8add5dc5f205f500d02d
Michael Wimble <mike@wimblerobotics.com>
2024-11-26 18:27:43 -0800

    Snapshot

--- Docker/runDocker.sh
+++ Docker/runDocker.sh
@@ -12,4 +12,4 @@ docker run \
     --user ros \
     rosjazzy
 
-#docker run -it --device /dev/snd -e ALSADEV=hw:2,0 -v /dev/:/dev/ -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v /home/ros/s_ws:/home/ros/s_ws --workdir /home/ros/s_ws  --user 1000:1000 macros
+#docker run -it --device /dev/snd -e ALSADEV=hw:2,0 -v /dev/:/dev/ -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v /home/ros/sigyn_ws:/home/ros/sigyn_ws --workdir /home/ros/sigyn_ws  --user 1000:1000 macros
