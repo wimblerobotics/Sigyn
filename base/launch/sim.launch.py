0cc62b1a13d274795bbb700e970b4dc22ebc963c
Michael Wimble <mike@wimblerobotics.com>
2024-11-29 13:00:15 -0800

    Cleanup, more iteration macros for gazebo

--- base/launch/sim.launch.py
+++ base/launch/sim.launch.py
@@ -1,109 +1,140 @@
 import os
-import sys
 import xacro
-import yaml
 
-import launch
 import launch_ros.actions
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration, Command
+from launch.substitutions import LaunchConfiguration, Command, PathJoinSubstitution
 from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
 from ament_index_python.packages import get_package_share_directory
 
 
 def generate_launch_description():
-  use_sim_time = LaunchConfiguration('use_sim_time')
-  base_pgk = get_package_share_directory("base")
-  description_pkg = get_package_share_directory("description")
-  xacro_file = os.path.join(description_pkg, 'urdf', 'base.urdf.xacro')
+    use_sim_time = LaunchConfiguration('use_sim_time')
+    base_pgk = get_package_share_directory("base")
+    description_pkg = get_package_share_directory("description")
+    xacro_file = os.path.join(description_pkg, 'urdf', 'sigyn.urdf.xacro')
 
-  robot_description_config = Command(['xacro ', xacro_file, ' use_ros2_control:=', 'false', ' sim_mode:=', use_sim_time])
-  robot_description_raw = xacro.process_file(xacro_file).toxml()
+    robot_state_publisher_node = launch_ros.actions.Node(
+        executable='robot_state_publisher',
+        output='screen',
+        package='robot_state_publisher',
+        parameters=[{'robot_description': xacro.process_file(
+          xacro_file,
+          mappings={'use_ros2_control': 'false', 'sim_mode': 'true'}).toxml()}],
+    )
 
-  robot_state_publisher_node = launch_ros.actions.Node(
-      executable='robot_state_publisher',
-      output='screen',
-      package='robot_state_publisher',
-      parameters=[{'robot_description': robot_description_raw,
-                    'use_sim_time': use_sim_time
-                    }]
-  )
-  
-  
-  default_world = os.path.join(
-    description_pkg,
-    'worlds',
-    'home.world'
-    # 'obstacles.world'
-    )    
+    default_world = os.path.join(
+        description_pkg,
+        'worlds',
+        'home.world'
+        # 'obstacles.world'
+    )
 
-  world = LaunchConfiguration('world')
+    world = LaunchConfiguration('world')
 
-  world_arg = DeclareLaunchArgument(
-      'world',
-      default_value=default_world,
-      description='World to load'
-      )
+    # Include the SLAM Toolbox launch file for mapping
+    slam_toolbox = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('slam_toolbox'), 'launch', 'online_async_launch.py')]),
+        launch_arguments={'use_sim_time': use_sim_time}.items()
+    )
+    world_arg = DeclareLaunchArgument(
+        'world',
+        default_value=default_world,
+        description='World to load'
+    )
 
-  # Include the Gazebo launch file, provided by the ros_gz_sim package
-  gazebo = IncludeLaunchDescription(
-              PythonLaunchDescriptionSource([os.path.join(
-                  get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')]),
-                  launch_arguments={'gz_args': ['-r -v4 ', world], 'on_exit_shutdown': 'true'}.items()
-            )
+    # Include the Gazebo launch file, provided by the ros_gz_sim package
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')]),
+        launch_arguments={'gz_args': [
+            '-r -v4 ', world], 'on_exit_shutdown': 'true'}.items()
+    )
 
-  # Run the spawner node from the ros_gz_sim package. The entity name doesn't really matter if you only have a single robot.
-  spawn_entity = Node(package='ros_gz_sim', executable='create',
-                      arguments=['-topic', 'robot_description',
-                                  '-name', 'sigyn',
-                                  '-x', '7.3',
-                                  '-y', '0.0',
-                                  '-z', '0.1'],
-                      output='screen')
+    # Run the spawner node from the ros_gz_sim package. The entity name doesn't really matter if you only have a single robot.
+    spawn_entity = Node(package='ros_gz_sim', executable='create',
+                        arguments=['-topic', 'robot_description',
+                                   '-name', 'sigyn',
+                                   '-x', '7.3',
+                                   '-y', '0.0',
+                                   '-z', '0.1'],
+                        output='screen')
 
-  diff_drive_spawner = Node(
-      package="controller_manager",
-      executable="spawner",
-      arguments=["diff_cont"],
-  )
+    diff_drive_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["diff_cont"],
+    )
 
-  joint_broad_spawner = Node(
-      package="controller_manager",
-      executable="spawner",
-      arguments=["joint_broad"],
-  )
+    joint_broad_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_broad"],
+    )
 
+    bridge_params = os.path.join(base_pgk, 'config', 'gz_bridge.yaml')
+    ros_gz_bridge = Node(
+        package="ros_gz_bridge",
+        executable="parameter_bridge",
+        arguments=[
+            '--ros-args',
+            '-p',
+            f'config_file:={bridge_params}',
+        ]
+    )
 
-  bridge_params = os.path.join(base_pgk,'config','gz_bridge.yaml')
-  ros_gz_bridge = Node(
-      package="ros_gz_bridge",
-      executable="parameter_bridge",
-      arguments=[
-          '--ros-args',
-          '-p',
-          f'config_file:={bridge_params}',
-      ]
-  )
+    ros_gz_image_bridge = Node(
+        package="ros_gz_image",
+        executable="image_bridge",
+        arguments=["/camera/image_raw"]
+    )
 
-  # ros_gz_image_bridge = Node(
-  #     package="ros_gz_image",
-  #     executable="image_bridge",
-  #     arguments=["/camera/image_raw"]
-  # )
+    # slam_toolbox_mapper = IncludeLaunchDescription(
+    #           PythonLaunchDescriptionSource([os.path.join(
+    #               get_package_share_directory('base'), 'launch', 'lifelong.launch.py')]),
+    #               # launch_arguments={'gz_args': ['-r -v4 ', world], 'on_exit_shutdown': 'true'}.items()
+    #         )
 
-  return launch.LaunchDescription([
-    DeclareLaunchArgument(
-      'use_sim_time',
-      default_value='true',
-      description='Use sim time if true'),
-    world_arg,
-    gazebo,
-    robot_state_publisher_node,
-    spawn_entity,
-    diff_drive_spawner,
-    joint_broad_spawner,
-    ros_gz_bridge,
-    # ros_gz_image_bridge
-  ])
+    # Bring up the navigation stack.
+    navigation_launch_path = PathJoinSubstitution(
+        [FindPackageShare('nav2_bringup'), 'launch', 'bringup_launch.py']
+    )
+
+    base_directory_path = get_package_share_directory('base')
+    map_path = os.path.join(base_directory_path, 'maps', 'snowberry4v31.yaml')
+    nav2_config_path = os.path.join(
+        base_directory_path, 'config', 'navigation.yaml')
+
+    nav2_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(navigation_launch_path),
+        launch_arguments={
+            'autostart': 'True',
+            'map': map_path,
+            'params_file': nav2_config_path,
+            'slam': 'False',
+            'use_composition': 'True',
+            'use_respawn': 'True',
+            # 'use_sim_time': 'false',
+        }.items()
+    )
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='true',
+            description='Use sim time if true'),
+        world_arg,
+        gazebo,
+        nav2_launch,
+        robot_state_publisher_node,
+        # slam_toolbox_mapper,
+        spawn_entity,
+        diff_drive_spawner,
+        joint_broad_spawner,
+        ros_gz_bridge,
+        ros_gz_image_bridge
+    ])
--- description/launch/description.launch.py
+++ description/launch/description.launch.py
@@ -1,64 +1,139 @@
+# Copyright (c) 2024 Michael Wimble
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 import os
 import xacro
-import yaml
 
-import launch
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, OpaqueFunction
-from launch.conditions import IfCondition
-import launch_ros.actions
+from launch.actions import DeclareLaunchArgument, ExecuteProcess
+from launch.conditions import IfCondition, UnlessCondition
+from launch_ros.actions import Node
 from launch.substitutions import LaunchConfiguration
 from ament_index_python.packages import get_package_share_directory
+from launch import LaunchContext
+from launch.actions import OpaqueFunction
 
-from launch.actions import LogInfo
-from launch.conditions import LaunchConfigurationEquals, LaunchConfigurationNotEquals
 
-def generate_launch_description():
-    
-    publish_joints = LaunchConfiguration('publish_joints')
-    use_sim_time = LaunchConfiguration('use_sim_time')
-
-    ld = LaunchDescription()
+def launch_robot_state_publisher(context, file_name_var):
     description_directory_path = get_package_share_directory('description')
-
-    ld.add_action(DeclareLaunchArgument(
-        name='publish_joints', 
-        default_value='true',
-        description='Launch joint_states_publisher if true'))
-
-    ld.add_action(DeclareLaunchArgument(
-        name='use_sim_time', 
-        default_value='false',
-        description='Use simulation (Gazebo) clock if true'))
-    
-    
-    # Publish joints.
-    joint_state_publisher_node = launch_ros.actions.Node(
-        package='joint_state_publisher',
-        executable='joint_state_publisher',
-        name='joint_state_publisher',
-        condition=IfCondition(LaunchConfiguration("publish_joints")),
-        parameters=[
-            {
-                'use_sim_time': use_sim_time
-            }
-        ]
-    )
-    ld.add_action(joint_state_publisher_node)
-
-    # Publish robot state and URDF.
-    urdf_path = os.path.join(description_directory_path, 'urdf/', 'base.urdf.xacro')
-    processed_urdf = xacro.process_file(urdf_path).toxml()
-    robot_state_publisher_node = launch_ros.actions.Node(
+    file_name = context.perform_substitution(file_name_var)
+    print(F"file_name: {file_name}")
+    xacro_file_path = os.path.join(
+        description_directory_path, 'urdf', file_name)
+    print(F"xacro_file_path: {xacro_file_path}")
+    urdf_as_xml = xacro.process_file(xacro_file_path).toxml()
+    robot_state_publisher_node = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         parameters=[
             {
-                'robot_description': processed_urdf,
-                'use_sim_time': use_sim_time
+                'frame_prefix': '',
+                'ignore_timestamp': False,
+                'publish_frequency': 30.0,
+                # processed_urdf,
+                'robot_description':  urdf_as_xml
             }
         ]
     )
-    ld.add_action(robot_state_publisher_node)
+    return [robot_state_publisher_node]
+
+
+def generate_launch_description():
+    launch_args = [DeclareLaunchArgument(
+        'urdf_file_name', default_value='xxx.urdf', description='URDF file name')]
+
+    do_rviz = LaunchConfiguration('do_rviz')
+    gui = LaunchConfiguration('gui')
+    publish_joints = LaunchConfiguration('publish_joints')
+    urdf_file_name = LaunchConfiguration('urdf_file_name')
+    use_sim_time = LaunchConfiguration('use_sim_time')
+
+    ld = LaunchDescription(launch_args)
+
+    description_directory_path = get_package_share_directory('description')
+    rviz_directory_path = get_package_share_directory('rviz')
+
+    ld.add_action(DeclareLaunchArgument(
+        name='do_rviz',
+        default_value='true',
+        description='Launch RViz if true'))
+    ld.add_action(DeclareLaunchArgument(
+        name='gui',
+        default_value='False',
+        description='Flag to enable joint_state_publisher_gui'))
+    ld.add_action(DeclareLaunchArgument(
+        name='publish_joints',
+        default_value='True',
+        description='Launch joint_states_publisher if true'))
+
+    ld.add_action(DeclareLaunchArgument(
+        name='urdf_file_name',
+        default_value='base.urdf.xacro',
+        description='URDF file name'))
+
+    ld.add_action(DeclareLaunchArgument(
+        name='use_sim_time',
+        default_value='false',
+        description='Use simulation (Gazebo) clock if true'))
+
+    # Publish joints.
+    joint_state_publisher_node = Node(
+        package='joint_state_publisher',
+        condition=UnlessCondition(gui),
+        executable='joint_state_publisher',
+        name='joint_state_publisher',
+        ### condition=IfCondition(LaunchConfiguration("publish_joints")),
+        # parameters=[
+        #     {
+        #         'delta': 0.0,
+        #         'publish_default_efforts': False,
+        #         'publish_default_positions': True,
+        #         'publish_default_velocities': False,
+        #         'rate': 30.0,
+        #         'use_mimic_tag': True,
+        #         'use_smallest_joint_limits': True
+        #     }
+        # ]
+    )
+    ld.add_action(joint_state_publisher_node)
+
+    ld.add_action(Node(
+        condition=IfCondition(gui),
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        name='joint_state_publisher_gui'))
+
+    ld.add_action(OpaqueFunction(function=launch_robot_state_publisher, args=[
+        LaunchConfiguration('urdf_file_name')]))
+    
+    echo_action = ExecuteProcess(
+        cmd=['echo', 'Rviz config file path: ' +
+             os.path.join(rviz_directory_path, 'config', 'config.rviz')],
+        output='screen'
+    )
+    ld.add_action(echo_action)
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        condition=IfCondition(LaunchConfiguration("do_rviz")),
+        arguments=[
+            '-d', os.path.join(rviz_directory_path, 'config', 'config.rviz')],
+    )
+    ld.add_action(rviz_node)
 
     return ld
