b60e6873fee7f1c00f5e4fdbc083a699f825d8d2
Michael Wimble <mike@wimblerobotics.com>
2025-01-02 14:07:10 -0800

    From calibration

--- teensy_monitor/tm5/config.h
+++ teensy_monitor/tm5/config.h
@@ -18,4 +18,4 @@ typedef struct RobotSpecs {
 static const MotorSpecs SuperDroid_1831 = {
     .max_rpm = 122, .motor_operating_voltage = 24, .motor_power_max_voltage = 24, .quad_pulses_per_revolution = 1000};
 
-static const RobotSpecs Sigyn_specs = {.wheel_diameter = 0.100, .wheels_separation = 0.3906};
+static const RobotSpecs Sigyn_specs = {.wheel_diameter = 0.102224144529039, .wheels_separation = 0.3906};
