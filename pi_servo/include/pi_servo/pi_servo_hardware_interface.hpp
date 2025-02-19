d63243a871f7bbaefe720d14e4f922493d0c0c78
Michael Wimble <mike@wimblerobotics.com>
2025-02-19 00:11:02 -0800

    Snapshot

---
+++ pi_servo/include/pi_servo/pi_servo_hardware_interface.hpp
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "hardware_interface/actuator_interface.hpp"
+#include "hardware_interface/handle.hpp"
+#include "hardware_interface/hardware_info.hpp"
+#include "hardware_interface/types/hardware_interface_return_values.hpp"
+#include "rclcpp/macros.hpp"
+
+namespace pi_servo
+{
+class PiServo : public hardware_interface::ActuatorInterface
+{
+public:
+  RCLCPP_SHARED_PTR_DEFINITIONS(PiServo)
+
+  hardware_interface::CallbackReturn on_init(
+    const hardware_interface::HardwareInfo & info) override;
+
+  hardware_interface::CallbackReturn on_configure(
+    const rclcpp_lifecycle::State & previous_state) override;
+
+  hardware_interface::CallbackReturn on_activate(
+    const rclcpp_lifecycle::State & previous_state) override;
+
+  hardware_interface::CallbackReturn on_deactivate(
+    const rclcpp_lifecycle::State & previous_state) override;
+
+  hardware_interface::return_type read(
+    const rclcpp::Time & time, const rclcpp::Duration & period) override;
+
+  hardware_interface::return_type write(
+    const rclcpp::Time & time, const rclcpp::Duration & period) override;
+
+private:
+  double hw_start_sec_;
+  double hw_stop_sec_;
+  double hw_slowdown_;
+  double command_;
+  double position_;
+};
+
+}  // namespace pi_servo
