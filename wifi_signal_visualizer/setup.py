3aa8743b1f99e238fbbc4b5ab81f6d2cd0e1c036
Michael Wimble <mike@wimblerobotics.com>
2025-03-24 22:43:43 -0700

    Snapshot producing fake random values for visualization

---
+++ wifi_signal_visualizer/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+package_name = 'wifi_signal_visualizer'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    py_modules=[],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ros',
+    maintainer_email='mike@wimblerobotics.com',
+    description='rviz2 visualizer of wifi signal strength',
+    license='MIT',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'wifi_signal_visualizer_node = wifi_signal_visualizer.wifi_signal_visualizer_node:main',
+        ],
+    },
+)
