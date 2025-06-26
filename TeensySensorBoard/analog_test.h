364c8446b53499c292ba4f3e665ff3114c263a67
Michael Wimble <mike@wimblerobotics.com>
2025-06-25 21:44:06 -0700

    Snapshot

--- TeensySensorBoard/analog_test.h
+++ TeensySensorBoard/analog_test.h
@@ -1,20 +1,42 @@
 #pragma once
 #include <Arduino.h>
+
 #include "test_module.h"
 
-class TAnalogTest : TTestModule{
+class TAnalogTest : public TTestModule {
  public:
   static TAnalogTest& singleton();
 
-  void setup() override {}
-//   void loop();
+  const char* deviceDescription(uint8_t deviceIndex);
 
-  int16_t getValueTenthsC(uint8_t device);
+  const char* deviceName(uint8_t deviceIndex);
 
- private:
+  const int numberOfDevices();
+
+  const int numberOfTestsPerDevice();
+
+  bool runTest(uint8_t testDevice, uint8_t testIndex);
+
+  const char* testName(uint8_t testIndex);
+
+  int16_t value(uint8_t deviceIndex);
+
+ protected:
   TAnalogTest();
   static TAnalogTest* g_singleton;
 
-  static int16_t g_analog0Value;
-  static int16_t g_analog1Value;
+  void setup();
+
+ private:
+  // GPIO addresses of analog sensors.
+  enum {
+    ANALOG_0_PIN = 24,
+    ANALOG_1_PIN = 25,
+    ANALOG_2_PIN = 26,
+    ANALOG_3_PIN = 27,
+    ANALOG_4_PIN = 38,
+    ANALOG_5_PIN = 39,
+    ANALOG_6_PIN = 40,
+    ANALOG_7_PIN = 41
+  };
 };
