5fddf8cd186fd03940a8fb7f9eb52bb956801a11
Michael Wimble <mike@wimblerobotics.com>
2025-08-06 11:11:14 -0700

    New database, battery simulator

--- perimeter_roamer_v3/scripts/battery_simulator.py
+++ perimeter_roamer_v3/scripts/battery_simulator.py
@@ -10,7 +10,7 @@ class BatterySimulator(Node):
         super().__init__('battery_simulator')
         self.publisher = self.create_publisher(BatteryState, '/battery_state', 10)
         self.timer = self.create_timer(1.0, self.publish_battery_state)
-        self.battery_level = 30.0  # Start at 100%
+        self.battery_level = 100.0  # Start at 100%
         self.get_logger().info('Battery simulator started')
 
     def publish_battery_state(self):
