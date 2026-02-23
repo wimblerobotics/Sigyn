e41193e6bc6522bbb14bc78fb937ddd60bc7d66b
Michael Wimble <mike@wimblerobotics.com>
2026-02-22 18:16:45 -0800

    remove: delete TeensyV2 (superseded by wimblerobotics/sigyn_teensy_boards)

--- TeensyV2/pio_extra_flags.py
+++
@@ -1,4 +0,0 @@
-Import("env")
-
-# Suppress upstream Teensy core C++ warnings without affecting C compilation.
-env.Append(CXXFLAGS=["-Wno-deprecated-copy"])
--- TeensyV2/test/serial_manager_stub_impl.cpp
+++
@@ -1,4 +0,0 @@
-// See note in arduino_mock_impl.cpp: PlatformIO's native test build may not
-// compile nested test subfolders, so include the stub implementation here.
-
-#include "mocks/serial_manager_stub.cpp"
