c08ce1aca99cfafe55eeddb01bc2a4762e6e5937
Michael Wimble <mike@wimblerobotics.com>
2026-03-11 18:31:33 -0700

    Fix: update sigyn_to_teensy -> wr_ros_teensy in launch_sigyn_to_sensor

--- sigyn_bringup/launch/sigyn.launch.py
+++ sigyn_bringup/launch/sigyn.launch.py
@@ -550,10 +550,10 @@ def generate_launch_description():
     def launch_sigyn_to_sensor(context, *args, **kwargs):
         if use_sim_time.perform(context).lower() == 'true':
             return []
-        pkg = get_package_share_directory("sigyn_to_teensy")
+        pkg = get_package_share_directory("wr_ros_teensy")
         return [IncludeLaunchDescription(
             PythonLaunchDescriptionSource(
-                os.path.join(pkg, "launch", "sigyn_to_teensy.launch.py")
+                os.path.join(pkg, "launch", "teensy_bridge.launch.py")
             ),
             launch_arguments={'namespace': 'sigyn'}.items(),
         )]
